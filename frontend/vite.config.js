ab5cf2db88d8ff1d291fe48059fecfdb954d8c0f
xchen34 <xchen@live.fr>
2026-07-04 15:32:22 +0200

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
-import { cloudflare } from "@cloudflare/vite-plugin";
-
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,9 +9,8 @@ const __dirname = path.dirname(__filename);
 const apiProxyTarget =
   process.env.VITE_API_PROXY_TARGET || "http://localhost:3000";
 
- 
 export default defineConfig({
-  plugins: [cloudflare({ viteEnvironment: { name: "ssr" } }), react()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
